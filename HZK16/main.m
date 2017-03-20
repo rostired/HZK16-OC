98b5ca2b3ff821ee4bdfd6ab0bee9d30aa8e9808
yelingbo <ylingb@gmail.com>
2017-03-20 22:24:11 +0800

    Initial Commit

---
+++ HZK16/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HZK16
+//
+//  Created by 叶凌波 on 17/3/20.
+//  Copyright © 2017年 rostired. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
